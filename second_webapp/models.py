6d5e6978929670b250f5f90e02d85e5901c0fa28
OdilovDeveloper <odilovcodes@gmail.com>
2025-07-24 11:35:12 +0500

    first commit

--- second_webapp/models.py
+++ second_webapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='second_webapp/')
+    image = CloudinaryField('image')  # Cloudinary orqali yuklanadi
 
     def __str__(self):
         return self.title
--- second_webapp/serializers.py
+++ second_webapp/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url.replace("http://", "https://")
+        return None
